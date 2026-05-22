54be6a78fcf1e3c8428d0cc361d5e18082c1497a
Nikhil-dev459 <NKDchamp004@gmail.com>
2026-05-22 18:41:45 +0530

    Basic setup done

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+
+const {serverConfig}=require('./config');
+const apiRoutes=require('./routes');
+
+const app=express();
+
+app.use(express.urlencoded({extended:true}));   //makes sure to read the url encoded stuff
+
+app.use('/api',apiRoutes);
+
+app.listen(serverConfig.PORT,()=>{
+    console.log(`Successfully started the server on PORT : ${serverConfig.PORT}`);
+    //Logger.info("Successfully started the server","root",{});
+});
